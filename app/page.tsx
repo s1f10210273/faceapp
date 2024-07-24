b5b8e01ebbf9986458ffbb3aafb11e466b247521
s1f10210273 <s1f102102739@iniad.org>
2024-07-24 13:11:23 +0900

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import React, { Suspense, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import TakePicture from './components/TakePicture';
 import Loading from './components/Loading';
-import { Box, Flex, Text, Image, Alert, AlertIcon, AlertTitle, AlertDescription, VStack, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, useDisclosure, Button, Input, FormControl, FormLabel } from '@chakra-ui/react';
+import { Box, Flex, Text, Image, Alert, AlertIcon, AlertTitle, AlertDescription, VStack, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, useDisclosure, Button } from '@chakra-ui/react';
 
 export default function Home() {
   const router = useRouter();
@@ -13,7 +13,6 @@ export default function Home() {
   const [responseData, setResponseData] = useState<{ image: string; message: string; predicted_age: number } | null>(null);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | null>(null);
-  const [manualAge, setManualAge] = useState<number | null>(null);
 
   const handleCaptureImage = async (capturedImageSrc: string) => {
     setCamOn(false);
@@ -35,28 +34,26 @@ export default function Home() {
         const data = await response.json();
         setResponseData(data);
       } else if (response.status === 500) {
-        setResponseData({ image: imageSrc, message: 'Error occurred', predicted_age: 0 });
+        setResponseData({ image: imageSrc, message: 'Error occurred', predicted_age: 55 });
       } else {
         throw new Error('Failed to fetch');
       }
     } catch (error) {
       console.error('Error uploading image', error);
-      setResponseData({ image: imageSrc, message: 'Error occurred', predicted_age: 0 });
+      setError('画像のアップロード中にエラーが発生しました。');
     } finally {
       setLoading(false);
       onOpen();
-    }
-  };
 
-  const handleAgeSubmit = () => {
-    onClose();
-    setTimeout(() => {
-      if (manualAge && manualAge > 60) {
-        router.push('/over60');
-      } else {
-        router.push('/young');
-      }
-    }, 3000); // 3秒後にページ遷移
+      setTimeout(() => {
+        onClose();
+        if (responseData?.predicted_age! > 60) {
+          router.push('/over60');
+        } else {
+          router.push('/young');
+        }
+      }, 3000);
+    }
   };
 
   return (
@@ -86,19 +83,7 @@ export default function Home() {
                     <VStack spacing={4} align="center" bg="white" p={6} rounded="lg" shadow="lg" textAlign="center">
                       <Text size="5xl" fontWeight="bold">こんにちは</Text>
                       <Image src={responseData.image} alt="Processed" maxW="full" borderRadius="md" style={{ borderRadius: '8px' }} />
-                      {responseData.predicted_age === 0 ? (
-                        <FormControl>
-                          <FormLabel>年齢を入力してください</FormLabel>
-                          <Input
-                            type="number"
-                            value={manualAge ?? ''}
-                            onChange={(e) => setManualAge(parseInt(e.target.value))}
-                          />
-                          <Button mt={4} colorScheme="blue" onClick={handleAgeSubmit}>送信</Button>
-                        </FormControl>
-                      ) : (
-                        <Text fontSize="lg">あなたは{responseData.predicted_age}代です</Text>
-                      )}
+                      <Text fontSize="lg">あなたは{responseData.predicted_age}代です</Text>
                     </VStack>
                   )}
                 </Suspense>
@@ -110,3 +95,4 @@ export default function Home() {
     </Box>
   );
 }
+
